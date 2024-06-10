2974eac22c83db951d3c1022e815603b8f6cad78
Prathamesh-Pulkanthwar <Pulkanthwarprathamesh2@gmail.com>
2024-06-10 22:48:27 +0530

    Enemy Follow the player

---
+++ Sumo Battle/Assets/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 3.0f;
+
+    private Rigidbody rb;
+    private GameObject player;
+
+    private void Start()
+    {
+        rb = GetComponentInChildren<Rigidbody>();
+        player = GameObject.Find("Player_Sphere");
+    }
+
+    private void Update()
+    {
+        Vector3 lookdirection = (player.transform.position - transform.position).normalized;
+        rb.AddForce( lookdirection * speed);
+    }
+}

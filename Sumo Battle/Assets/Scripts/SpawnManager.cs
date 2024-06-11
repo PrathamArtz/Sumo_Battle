bab1091639d4ddcf8e5ad5b3a17d126f728b59f5
Prathamesh-Pulkanthwar <Pulkanthwarprathamesh2@gmail.com>
2024-06-11 23:30:01 +0530

    Enemy Wave

--- Sumo Battle/Assets/Scripts/SpawnManager.cs
+++ Sumo Battle/Assets/Scripts/SpawnManager.cs
@@ -8,13 +8,28 @@ public class SpawnManager : MonoBehaviour
 
     private float spawnrange = 9.0f;
 
-     void Start()
+    void Start()
     {
+        SpawnEnemyWave();
+
+    }
+
+    void SpawnEnemyWave()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            Instantiate(enemyPrefab, GenrateRandomPosition(), enemyPrefab.transform.rotation);
+        }
+    }
+
+    private Vector3 GenrateRandomPosition()
+    {
+
         float spawnX = Random.Range(-spawnrange, spawnrange);
         float spawnZ = Random.Range(-spawnrange, spawnrange);
 
         Vector3 randomPos = new Vector3(spawnX, 0, spawnZ);
-
-        Instantiate(enemyPrefab, randomPos , enemyPrefab.transform.rotation);
+        return randomPos;
     }
+
 }
